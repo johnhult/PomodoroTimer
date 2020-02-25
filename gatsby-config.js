7fde12c3a2b5e9d03fac32f3d5fe5699dd1ded3d
John Hult <johnhult89@gmail.com>
2020-02-25 21:58:01 +0100

    added PWA capability

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,8 +44,26 @@ module.exports = {
         // Add any options here
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Pomodoro Time",
+        short_name: "Pomodoro Time",
+        start_url: "/",
+        background_color: "#e45030",
+        theme_color: "#e45030",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
